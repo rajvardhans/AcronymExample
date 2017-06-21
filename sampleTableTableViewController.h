4ec7b340b822038766bbfcb138161057e0e70508
rajvardhans <rjvrdhnrdd682@gmail.com>
2017-06-20 21:43:49 -0400

    sampleTableView

---
+++ sampleTableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  sampleTableTableViewController.h
+//  
+//
+//  Created by Rajavardhan Soma on 6/20/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sampleTableTableViewController : UITableViewController
+
+@end
